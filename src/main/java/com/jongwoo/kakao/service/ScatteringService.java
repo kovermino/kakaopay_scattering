0593a72a66c2c6e26766548e746e79a6ac59e9cf
JongwooYoon <koverwhelm@gmail.com>
2020-09-21 00:06:16 +0900

    ScatteringService / scattering method 작성

--- src/main/java/com/jongwoo/kakao/service/ScatteringService.java
+++ src/main/java/com/jongwoo/kakao/service/ScatteringService.java
@@ -1,17 +1,38 @@
 package com.jongwoo.kakao.service;
 
+
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+import com.jongwoo.kakao.util.MoneyDistributer;
 
 @Component
 @Service
 public class ScatteringService {
 	
-	public String scattering(String userId, String roomId, String totalAmount, int distributeAmount) {
-		// make money dist
-		// insert money dist
-		// return token
-		return "";
+	public Map<String, String> scattering(final String userId, 
+							 final String roomId, 
+							 final String totalAmount, 
+							 final String distributeCount) {
+		Map<String, String> responseEntity = new HashMap<>();
+		try {
+			final int ta = Integer.parseInt(totalAmount);
+			final int da = Integer.parseInt(distributeCount);
+			
+			MoneyDistributer md = new MoneyDistributer();
+			final int[] dist = md.distributeMoney(ta, da);
+			responseEntity.put("token", "STF");
+		} catch (NumberFormatException nfe) {
+			responseEntity.put("error_msg", "Parameters must be integer value.");
+		} catch (IllegalArgumentException iae) {
+			responseEntity.put("error_msg", iae.toString());
+		} catch (Exception e) {
+			responseEntity.put("error_msg", e.toString());
+		}
+		return responseEntity;
 	}
 	
 	public int receive(String userId, String roomId, String token) {
