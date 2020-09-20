c17b6d6f26406b72ac799f6a145b869cc83a4cfd
JongwooYoon <koverwhelm@gmail.com>
2020-09-21 00:17:48 +0900

    MoneyScattering / Entity, Repository 작성

---
+++ src/main/java/com/jongwoo/kakao/repository/MoneyScatteringRepository.java
@@ -0,0 +1,9 @@
+package com.jongwoo.kakao.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.jongwoo.kakao.vo.MoneyScattering;
+
+public interface MoneyScatteringRepository extends CrudRepository<MoneyScattering, Long>{
+
+}
--- src/main/java/com/jongwoo/kakao/vo/MoneyScattering.java
+++ src/main/java/com/jongwoo/kakao/vo/MoneyScattering.java
@@ -1,20 +1,43 @@
 package com.jongwoo.kakao.vo;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.Date;
 
-public class MoneyScattering {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="MONEY_SCATTERING")
+public class MoneyScattering implements Serializable{
 	
+	private static final long serialVersionUID = 7355909043814360604L;
+	
+	@Id 
+	@GeneratedValue(strategy=GenerationType.AUTO) 
+	@Column(name="sct_seq") 
+	private long sctSeq;
+
+	@Column(name="token") 
 	String token;
 	
+	@Column(name="owner") 
 	String owner;
 	
+	@Column(name="room_id") 
 	String roomId;
 	
-	BigDecimal totalAmount;
+	@Column(name="total_amount") 
+	int totalAmount;
 	
+	@Column(name="distribute_count") 
 	int distributeCount;
 	
+	@Column(name="time") 
 	Date time;
 
 }
